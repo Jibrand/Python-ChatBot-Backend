09034b646c232cab1a6e2c5416b51c91b81b17e9
Muhammad Jibran <137154043+Jibrand@users.noreply.github.com>
2023-12-20 10:53:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,20 @@ app = Flask(__name__)
 openai.api_key = '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'
 
  
-story = """Once upon a time in a small village, there lived a young girl named Lily"""
+story = """Once upon a time in a small village, there lived a young girl named Lily. 
+She was known for her love of nature and spent her days exploring the outskirts of the village. 
+One day, while wandering through the dense forest, Lily stumbled upon a hidden path that led her to a mysterious garden. 
+The garden was unlike anything Lily had ever seen. Vibrant flowers of every color imaginable bloomed in perfect harmony, 
+and the air was filled with the sweet scent of blossoms. Intrigued, Lily decided to explore further and discovered a small pond at the heart of the garden. 
+As she approached, a magical creature emerged from the water – a talking frog named Oliver. 
+Oliver explained that the garden was enchanted, and only those with pure hearts could find it. 
+Lily, being kind-hearted and curious, was welcomed by the magical inhabitants of the garden. 
+She spent her days talking to the animals, learning about the secrets of the enchanted realm, and even befriending a wise old owl. 
+However, the enchantment came with a condition. Lily had to keep the garden a secret and not reveal its location to anyone with a wicked heart. 
+The magical beings explained that the garden's existence depended on its secrecy, and if it was exposed to the wrong person, the enchantment would fade away. 
+Days turned into weeks, and Lily's heart swelled with joy as she continued to cherish the magical haven. 
+But one day, a sly and selfish villager named Edgar overheard Lily talking about the enchanted garden. 
+Greed overtook him, and he decided to find it for himself, hoping to use its magic for personal gain."""
 
  
 def answer_question(question):

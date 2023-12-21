e4317e3d374b60b8027f0e97f614d7cfb73ab6b0
Muhammad Jibran <137154043+Jibrand@users.noreply.github.com>
2023-12-21 10:30:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,35 @@ CORS(app)
 openai.api_key = os.environ.get('openaii') #jibran
 
 
-story = """there was a boy whose name is jibran he is a mern stack developer"""
+#story = """there was a boy whose name is jibran he is a mern stack developer"""
  
+story = """
+Welcome to The IK Agency, your premier partner for cutting-edge Social Media Marketing and Management (SMMA) services. 
+At The IK Agency, we pride ourselves on delivering innovative solutions to enhance your online presence, engage your audience, and drive measurable results.
 
+About Us:
+The IK Agency is a team of dedicated professionals passionate about helping businesses thrive in the digital landscape. 
+With years of experience in the industry, we understand the dynamic nature of social media and leverage the latest trends to elevate your brand.
+
+Our Services:
+- Social Media Strategy: Tailored strategies to align with your business goals and target audience.
+- Content Creation: Compelling and shareable content that resonates with your audience.
+- Community Management: Building and nurturing your online community for sustained growth.
+- Analytics and Reporting: Data-driven insights to measure success and inform future strategies.
+
+Why Choose The IK Agency?
+- Proven Results: Our track record speaks for itself, with success stories across various industries.
+- Innovative Approach: Stay ahead of the competition with our forward-thinking strategies.
+- Client-Centric Focus: Your success is our priority, and we work closely with you to achieve your objectives.
+
+Contact Information:
+Phone: +852 6439 4061
+Email: info@theikagency.com
+Owner: Grace Kim
+
+Get in Touch:
+Ready to take your social media presence to the next level? Contact The IK Agency today for a consultation. Let's turn your social media channels into powerful tools for growth.
+"""
  
 def answer_question(question):
     

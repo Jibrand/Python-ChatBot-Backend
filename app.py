250e9ed8a30b95b226ea3fb8722ab90b4bf32cf2
Muhammad Jibran <137154043+Jibrand@users.noreply.github.com>
2023-12-22 23:51:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,34 @@ CORS(app)
 openai.api_key = os.environ.get('openaii') #jibran
 
 
-#story = """there was a boy whose name is jibran he is a mern stack developer"""
+ story1 = """
+name of company is WebTose 
+Welcome to WebTose – Where Digital Dreams Come to Life!
+
+In the heart of innovation, WebTose stands as a beacon for businesses seeking unparalleled excellence in website development and AI chatbot services. Let us take you on a journey of digital transformation, where your ideas evolve into captivating online experiences.
+
+Once upon a time, in the bustling realm of the internet, there was a visionary company named WebTose. With a team of dedicated professionals driven by a passion for technology, WebTose emerged as a leader in crafting bespoke websites and implementing cutting-edge AI chatbot solutions.
+
+Our Website Development Wizards:
+Picture a team of skilled developers at WebTose, weaving intricate codes to build the digital face of your brand. From sleek portfolios that dazzle to powerful e-commerce platforms that sell, our wizards specialize in bringing your website dreams to reality. Each website is a unique masterpiece, reflecting the essence of your brand and capturing the attention of your online audience.
+
+The AI Chatbot Alchemists:
+In the enchanted laboratory of WebTose, our AI Chatbot Alchemists work tirelessly to infuse intelligence into your customer interactions. These magical chatbots, tailored to your business needs, provide seamless and personalized experiences. Watch as automation transforms your customer support, providing instant responses and ensuring your users feel a connection with your brand.
+
+Why Choose the WebTose Adventure?
+- Expertise Beyond Boundaries: Our team of seasoned professionals possesses a wealth of experience in both website development and the realm of AI.
+- Pioneering Innovation: Navigate the digital landscape with confidence, as WebTose pioneers innovative solutions that keep you ahead of the curve.
+- Your Success, Our Priority: At WebTose, we thrive on your success. Our customer-centric approach ensures that every solution aligns with your goals and aspirations.
+
+Contacting the Digital Guides:
+📞 Phone: +923308138077
+📧 Email: info@webtose.com
+🌐 Website: www.webtose.com
+
+Embark on Your Digital Odyssey:
+Ready to embark on a digital adventure? Contact WebTose today for a consultation. Let's embark on a journey where your online presence transforms into a captivating story of success!
+"""
+
  
 story = """
 Welcome to The IK Agency, your premier partner for cutting-edge Social Media Marketing and Management (SMMA) services. 

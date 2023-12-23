4e492a92402d402f28c2a54a10402173d9e2c0c5
Muhammad Jibran <137154043+Jibrand@users.noreply.github.com>
2023-12-22 23:24:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
-from flask import Flask, request, jsonify
-import openai  # Import the OpenAI library
-from flask_cors import CORS 
+ from flask import Flask, request, jsonify
+import openai
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app) 
+CORS(app)
 
-openai.api_key = os.environ.get('openaii') #jibran
+openai.api_key = os.environ.get('openaii')  # Your OpenAI API key
 
-
-#story = """there was a boy whose name is jibran he is a mern stack developer"""
- 
-story = """
+ story = """
 Welcome to The IK Agency, your premier partner for cutting-edge Social Media Marketing and Management (SMMA) services. 
 At The IK Agency, we pride ourselves on delivering innovative solutions to enhance your online presence, engage your audience, and drive measurable results.
 
@@ -38,12 +35,11 @@ Owner: Grace Kim
 Get in Touch:
 Ready to take your social media presence to the next level? Contact The IK Agency today for a consultation. Let's turn your social media channels into powerful tools for growth.
 """
- 
+
+
 def answer_question(question):
-    
     prompt = f"The story is: {story}\nQuestion: {question}"
 
-   
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=prompt,
@@ -51,7 +47,6 @@ def answer_question(question):
         max_tokens=1000
     )
 
- 
     answer = response.choices[0].text.strip()
 
     prefixes_to_remove = ["?\n\nAnswer:", "?"]
@@ -59,40 +54,40 @@ def answer_question(question):
         if answer.startswith(prefix):
             answer = answer[len(prefix):].strip()
 
-   
+    # If OpenAI API doesn't return a satisfactory answer, use GPT-3.5 to generate a response
+    if not answer:
+        answer = generate_gpt_response(question)
+
     return answer
 
- 
+
+def generate_gpt_response(question):
+    # You can use the GPT-3.5 model here to generate a response for the question
+    # You may need to adapt this part based on your GPT-3.5 implementation
+    # For simplicity, you can use OpenAI's GPT-3 Python library
+    gpt_response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=question,
+        temperature=0.7,
+        max_tokens=1000
+    )
+
+    return gpt_response.choices[0].text.strip()
+
+
 @app.route('/', methods=['GET'])
 def start():
     print('Starting Server')
     return 'Server is running'
+
 
 @app.route('/ask', methods=['POST'])
 def ask_question():
     data = request.get_json()
     question = data.get('question', '')
     answer = answer_question(question)
-    return jsonify({ 'answer': answer})
- 
+    return jsonify({'answer': answer})
 
 
-
-
- 
-# from flask import Flask
-
-
-# app = Flask(__name__)
-
- 
-# @app.route('/')
-# # ‘/’ URL is bound with hello_world() function.
-# def hello_world():
-# 	return 'Hello World'
-
- 
- 
-
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
